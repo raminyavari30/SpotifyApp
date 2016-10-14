0a4db5d6e03704f98fe03bb2cbbed50507fc717d
Ramin <ramin_yavari03@yahoo.com>
2016-10-13 21:28:06 -0700

    Preparing to Deploy if possible

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express'); 
 
 var app = express();
+var port = process.env.PORT || 8888;
 require('dotenv').config();
 require('./middleware.js')(app, express);
 require('./routes.js')(app);
 
-console.log('Listening on 8888');
-app.listen(8888);
+app.listen(8888, function() {
+  console.log('Listening on 8888');
+});
